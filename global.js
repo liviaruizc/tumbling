47053d91e4a35eb38ba9d7bc1bb1c7326736910e
Livia Correia <liviacorreiia@gmail.com>
2025-04-27 14:01:05 -0400

    Update global.js

--- global.js
+++ global.js
@@ -3,13 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const user = JSON.parse(localStorage.getItem("loggedInUser"));
 
     if (user && logoutBtn) {
-        logoutBtn.style.display = "inline";
+        logoutBtn.style.display = "inline-block";
 
         logoutBtn.addEventListener("click", (e) => {
             e.preventDefault();
             localStorage.removeItem("loggedInUser");
-            window.location.href = "login.html";
-
+            window.location.href = "login.html"; // Redirect to login page
         });
+    } else if (logoutBtn) {
+        logoutBtn.style.display = "none"; // Hide Logout button if no user logged in
     }
 });
